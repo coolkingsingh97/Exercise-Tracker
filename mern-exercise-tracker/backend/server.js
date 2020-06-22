9b64e4daca5d629af8ea158ac577cede2216f595
kulraj0000 <kulraj0000@gmail.com>
2020-06-21 18:53:44 -0700

    Frontend and Connection

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -17,11 +17,12 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 const exercisesRouter = require('./routes/exercises');
-const users.Router = require('./routes/uesrs')
+const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);
-app.use('/users', usersRouter)
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
+
